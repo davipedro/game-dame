a864417b1587935525d731ade456e045ff4b6b1a
Davi Pedro <davisilvaps02@gmail.com>
2024-06-10 14:01:32 -0300

    feat: adicionando página de registro

--- game-dame/src/App.jsx
+++ game-dame/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Form from './components/Form'
 
 function App() {
   return (
     <>
-      
+      <div className="wrapper">
+        <div className="form-container">
+            <Form />
+        </div>
+      </div>
     </>
   )
 }
