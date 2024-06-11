1ac05ca9924eb38df64d17a1b9df4f70e517d933
Davi Pedro <davisilvaps02@gmail.com>
2024-06-11 20:04:17 -0300

    feat: adicionando tela de administrador

---
+++ game-dame/src/pages/AdminPanel.jsx
@@ -0,0 +1,87 @@
+import { useRef } from 'react'
+
+import '../components/Navbar'
+import Navbar from '../components/Navbar'
+
+import './AdminPanel.css'
+
+
+const AdminPanel = () => {
+
+    const fileInput = useRef(null)
+
+    const handleButtonClick = () => {
+        fileInput.current.click()
+    }
+
+    const handleFileChange = () => {
+        console.log(fileInput.current.files[0])
+    }
+
+
+  return (
+    <div>
+        <Navbar />
+        <main className='admin-panel-main-container'>
+            <section className='panel-section edge-section'>
+                <h2>Cadastrar Jogo</h2>
+                <input 
+                    type="text" 
+                    placeholder='Título do Jogo'
+                />
+                <input 
+                    type="text" 
+                    placeholder='Gênero'
+                />
+                <textarea 
+                    name="" id=""
+                    placeholder='Descrição'
+                />    
+                <input 
+                    className='file-input'
+                    type="file" accept='image/*'
+                    aria-label='hidden-file-input'
+                    ref={fileInput}
+                    onChange={handleFileChange}
+                />
+                <button type='button' className='file-btn' onClick={handleButtonClick}>Inserir Imagem</button>
+                <button type='submit' className='submit-btn'>Cadastrar</button>
+            </section>
+            <section className='panel-section middle-section'>
+                <h2>Cadastrar Jogo</h2>
+                <input 
+                    type="text" 
+                    placeholder='Título do jogo ou ID'
+                />
+                <button type='submit' className='submit-btn'>Remover</button>
+            </section>
+            <section className='panel-section edge-section'>
+                <h2>Cadastrar Jogo</h2>
+                <input 
+                    type="text" 
+                    placeholder='Título do Jogo'
+                />
+                <input 
+                    type="text" 
+                    placeholder='Gênero'
+                />
+                <textarea 
+                    name="" id=""
+                    placeholder='Descrição'
+                />    
+                <input 
+                    className='file-input'
+                    type="file" accept='image/*'
+                    aria-label='hidden-file-input'
+                    ref={fileInput}
+                    onChange={handleFileChange}
+                />
+                <button type='button' className='file-btn' onClick={handleButtonClick}>Inserir Imagem</button>
+                <button type='submit' className='submit-btn'>Modificar</button>
+            </section>
+        </main>
+    </div>
+  )
+}
+
+export default AdminPanel
