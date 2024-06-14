9d5fa0424e16eb7e95ec105d99db7fe8447c2d0b
Davi Pedro <davisilvaps02@gmail.com>
2024-06-14 00:59:55 -0300

    feat: adicionando routers

--- game-dame/src/App.jsx
+++ game-dame/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
-import Form from './components/Form'
+import UserPanel from './pages/UserPanel'
+import AccountRecovery from './pages/AccountRecovery'
+import AdminPanel from './pages/AdminPanel'
+import LoginForm from './pages/LoginForm'
+import RegisterForm from './pages/RegisterForm'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <div className="wrapper">
-        <div className="form-container">
-            <Form />
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<LoginForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/cadastro" element={<RegisterForm />} />
+        <Route path="/recuperar-conta" element={<AccountRecovery />} />
+        <Route path="/painel-usuario" element={<UserPanel />} />
+        <Route path="/painel-admin" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
