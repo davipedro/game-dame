9da25af03af9b01393b722c2c577dd495b0fd95d
Davi Pedro <davisilvaps02@gmail.com>
2024-06-14 01:02:26 -0300

    feat: finalizando painel usuário

--- game-dame/src/pages/UserPanel.jsx
+++ game-dame/src/pages/UserPanel.jsx
@@ -1,6 +1,6 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/navbar/Navbar"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import './UserPanel.css'
 
@@ -11,22 +11,48 @@ import spongeBob from '../assets/spongebob-image.png'
 import deliverUsMars from '../assets/deliver-us-mars-image.png'
 import godOfWar from '../assets/gow-image.png'
 import deathStranding from '../assets/death-stranding-image.png'
+import GameGallery from "../components/GameGallery/GameGallery"
 
 const UserPanel = () => {
+  
+  const gamesPerPageMobile = 1;
+  const gamesPerPageDesktop = 4;
 
   const [username, setUsername] = useState("Nome do Usuário")
   const [email, setEmail] = useState("nomeusuario@email.com")
   const [cpf, setCpf] = useState("123.456.789-00")
+  
+  const games = [
+    { image: spongeBob, rating: '100/100' },
+    { image: deliverUsMars, rating: '95/100' },
+    { image: godOfWar, rating: '75/100' },
+    { image: deathStranding, rating: 'Adicionar avaliação' },
+    { image: spongeBob, rating: '88/100' },
+    { image: deliverUsMars, rating: '92/100' },
+  ];
+
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 590);
+
+  const updateScreenSize = () => {
+    setIsSmallScreen(window.innerWidth <= 590);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', updateScreenSize);
+    return () => {
+      window.removeEventListener('resize', updateScreenSize);
+    };
+  }, []);
 
   return (
     <>
-      {/* <header id="shadow-box"></header> */}
       <Navbar />
+      <div id="box-shadow"></div>
       <main className="user-panel-main-container">
         <section className="user-data">
           <h1>Olá, {username}</h1>
-          <p>Seu email é {email}</p>
-          <p>Seu CPF é {cpf}</p>
+          <p id="display-email">Seu email é {email}</p>
+          <p id="display-cpf">Seu CPF é {cpf}</p>
 
           <div 
             className="user-panel-btn" 
@@ -47,31 +73,17 @@ const UserPanel = () => {
             id="admin-panel-link"
             onClick={() => {window.location.href = '/admin-panel'}}
           >
-            <a href="/admin-panel">Tela do admin</a>
+            <a href="/painel-admin">Tela do admin</a>
           </div>
         </section>
         <section className="user-games">
-          <div id="games-section-title">
-            <h2>Jogos Adquiridos</h2>
-          </div>
-          <ul className="user-games-library">
-            <li>
-              <img src={spongeBob} alt="jogo 1" />
-              <p className="classificação">100/100</p>
-            </li>
-            <li>
-              <img src={deliverUsMars} alt="jogo 1" />
-              <p className="classificação">95/100</p>
-            </li>
-            <li>
-              <img src={godOfWar} alt="jogo 1" />
-              <p className="classificação">75/100</p>
-            </li>
-            <li>
-              <img src={deathStranding} alt="jogo 1" />
-              <p className="classificação">Adicionar avaliação</p>
-            </li>
-          </ul>
+
+          {!isSmallScreen ? (
+            <GameGallery games={games} gamesPerPage={gamesPerPageDesktop} />
+          ) : (
+            <GameGallery games={games} gamesPerPage={gamesPerPageMobile} />
+          )}
+          
         </section>
         <a href="/cart" id="cart"></a>
       </main>
